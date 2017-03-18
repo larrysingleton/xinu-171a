3a3d67c75acd3833f75b0b90a4df55c3d40e2e70
Larry Singleton <larrysingleton007@gmail.com>
2017-03-18 10:27:47 -0500

    Add led.h

---
+++ include/led.h
@@ -0,0 +1,12 @@
+/* led.h - constants and structure for led device type */
+
+#define LED_ON 'Y'
+#define LED_OFF 'N'
+
+#define LED_OPEN 1
+#define LED_CLOSE 0
+
+struct leddevice {
+    int status;          /* 0 if not open, non-zero otherwise */
+    char illuminated;    /* Y if illuminated, N if not */
+}

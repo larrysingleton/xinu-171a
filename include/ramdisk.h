3948996d6ffee21702179ea0b78659b5ef923c94
Larry Singleton <larrysingleton007@gmail.com>
2017-04-15 16:34:58 -0500

    Add ramcache

--- include/ramdisk.h
+++ include/ramdisk.h
@@ -1,16 +1,33 @@
 /* ramdisk.h - definitions for a ram disk */
 
+/*---------------------*/
 /* Ram disk block size */
+/*---------------------*/
+#define	RM_BLKSIZ 512		/* block size */
+#define	RM_BLKS	200		/* number of blocks */
 
-#define	RM_BLKSIZ	512		/* block size			*/
-#define	RM_BLKS		200		/* number of blocks		*/
-
-#define RAM_CTL_SET   1
-#define RAM_CTL_ZERO  2
-#define RAM_CTL_READ  3
+/*-------------------*/
+/* Control functions */
+/*-------------------*/
+#define RAM_CTL_SET 1		/* set dist, smallcost, largecost */
+#define RAM_CTL_ZERO 2		/* set total simulated seek cost to 0 */
+#define RAM_CTL_READ 3		/* return total simulated seek cost */
 
 struct	ramdisk	{
-	char disk[RM_BLKSIZ * RM_BLKS];
+    char disk[RM_BLKSIZ * RM_BLKS];
 };
 
 extern struct ramdisk Ram;
+
+struct ramcache {
+    char block[RM_BLKSIZ];	/* a cached disk block */
+    int blockno;		/* block number of cached block */
+    int next;			/* index of next cached block, or -1 */
+};
+
+#define RM_CACHE 20		/* number of cached blocks */
+struct ramcache RamCache[RM_CACHE];
+uint32 RamCacheHead;	/* head of cache linked list, or -1 */
+uint32 RamCacheFree;	/* head of cache free list, or -1 */
+
+#define RAMDISKDEBUG 0
